360d126184835f44626b7f3055424176539940d9
jaydenb2008 <jjeromebui2008@gmail.com>
2024-07-09 15:41:01 -0700

    start html and css

---
+++ background.js
@@ -0,0 +1,4 @@
+const verse = document.getElementById("verse")
+
+const generatedVerse = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting lifeFor God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life";
+verse.textContent = generatedVerse;
